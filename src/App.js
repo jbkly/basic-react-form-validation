642afa5c3b7f66c7fa8fd8fc4a1a9462108022c7
Jordan Buckley <jordan@bkly.org>
2019-06-24 04:41:35 +0000

    start on logic for display of message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Form from './components/Form'
 import Message from './components/Message'
 
 class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isValid: false,
+      };
+    }
 
     render() {
         return (<div>
             <Form></Form>
-            <Message></Message>
+            <Message isValid={this.state.isValid}></Message>
         </div>);
     }
 }
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Message = () => {
+const Message = ({ isValid }) => {
     return (
         <div>
-            <h3 className="text-center message">Form is Incomplete!</h3>
+            <h3 className="text-center message">
+              Form is { isValid ? 'Complete' : 'Incomplete' }!
+            </h3>
         </div>
     )
 }
